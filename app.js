f3beddc585d89df0134afd610e43519234ac1b81
otajor <oturnermajor@gmail.com>
2018-01-20 09:56:31 +0000

    heroku port env

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+app.listen(process.env.PORT || 4000, () => console.log('Example app listening on port 4000!'))
--- db/createAccount.js
+++ db/createAccount.js
@@ -2,12 +2,12 @@ const knex = require('./client.js')
 
 const createAccount = ({ telephone, balance = 0, accountAddress }) =>
   knex('accounts')
-    .returning('*')
-    .insert({
-      telephone: telephone,
-      balance: balance,
-      account_address: accountAddress
-    })
-    .catch(console.log)
+  .returning('*')
+  .insert({
+    telephone: telephone,
+    balance: balance,
+    account_address: accountAddress
+  })
+  .catch(console.log)
 
-  module.exports = createAccount
+module.exports = createAccount
