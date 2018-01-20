a421c2fd07fb47c8cd17948a47503ce93ff6f4b6
rinoma <ganzo.s@libero.it>
2018-01-20 15:09:23 +0000

    added some fake transactions and accounts

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -1,5 +1,24 @@
-const accounts = [{
-
-}];
+const accounts = {
+ //phonenumber: {
+   // public Key
+   // balance
+   // Loan amount
+   // Verified: true
+ //},
+ 07479869730: {
+   publicKey: 673215838912,
+   balance: 550,
+   accountId: 8372
+   loanAmount: 500,
+   verified: true,
+ },
+ 07483942794: {
+   publicKey: 478362489327,
+   accountId: 3289
+   balance: 1050,
+   loanAmount: 700,
+   verified: true,
+ },
+};
 
 export default accounts;
--- src/data/transactions.js
+++ src/data/transactions.js
@@ -1,5 +1,42 @@
 const transactions = {
-  // accountId: []
+  // accountId: [
+    //{ Description:
+    // Datetime
+    // Amount
+    // recipient phonenumber},
+    //{ Description:
+    // Datetime
+    // Amount
+    // recipient phonenumber},
+  //]
+  07479869730: [
+    {
+      description: 'Invoice to Tesco 43782'
+      datetime: 2018-01-19T03:24:00,
+      amount: 34.67,
+      recpientPhoneNumber: 07483942794,
+    },
+    {
+      description: 'Payment ref. 43342'
+      datetime: 2018-01-19T04:21:20,
+      amount: 56.47,
+      recpientPhoneNumber: 07826138109,
+    },
+  ],
+  074837492432: [
+    {
+      description: 'Amazon Uk retail'
+      datetime: 2018-01-19T13:54:00,
+      amount: 5.99,
+      recpientPhoneNumber: 07382193718,
+    },
+    {
+      description: 'Starbucks store 43799'
+      datetime: 2018-01-19T14:45:30,
+      amount: 99.01,
+      recpientPhoneNumber: 07498327427,
+    },
+  ]
 };
 
 export default transactions;
