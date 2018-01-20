68f0aa44f4997c646e0e8a0eb300cf754f1e6c63
rinoma <ganzo.s@libero.it>
2018-01-20 15:10:59 +0000

    added some fake transactions and accounts

--- src/data/transactions.js
+++ src/data/transactions.js
@@ -12,13 +12,13 @@ const transactions = {
   07479869730: [
     {
       description: 'Invoice to Tesco 43782'
-      datetime: 2018-01-19T03:24:00,
+      datetime: '2018-01-19T03:24:00',
       amount: 34.67,
       recpientPhoneNumber: 07483942794,
     },
     {
       description: 'Payment ref. 43342'
-      datetime: 2018-01-19T04:21:20,
+      datetime: '2018-01-19T04:21:20',
       amount: 56.47,
       recpientPhoneNumber: 07826138109,
     },
@@ -26,13 +26,13 @@ const transactions = {
   074837492432: [
     {
       description: 'Amazon Uk retail'
-      datetime: 2018-01-19T13:54:00,
+      datetime: '2018-01-19T13:54:00',
       amount: 5.99,
       recpientPhoneNumber: 07382193718,
     },
     {
       description: 'Starbucks store 43799'
-      datetime: 2018-01-19T14:45:30,
+      datetime: '2018-01-19T14:45:30',
       amount: 99.01,
       recpientPhoneNumber: 07498327427,
     },
