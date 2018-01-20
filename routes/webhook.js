a810151fd65861d6b0bf9fc3aeb5c32040b2d394
otajor <oturnermajor@gmail.com>
2018-01-20 13:16:12 +0000

    steps added to webhook functions

--- routes/webhook.js
+++ routes/webhook.js
@@ -16,13 +16,23 @@ module.exports = (req, res) => {
 }
 
 const createAccount = ({ req, res, telephone }) => {
+  // 1. Create ETH wallet
+  // 2. Create DB entry linking phone to ETH wallet
+  // 3. Transfer 5 KT into new ETH wallet
+  // 3. Reply with success SMS stating new balance
   res.send('success')
 }
 
 const checkBalance = ({ req, res, telephone }) => {
+  // 1. Retrieve balance of ETH wallet
+  // 2. Reply with success SMS stating balance
   res.send('success')
 }
 
 const sendTransaction = ({ req, res, telephone, message }) => {
+  // 1. Check balance of ETH wallet (check if balance sufficient)
+  // 2. Send ETH from one wallet to the other
+  // 3. Update both DB entries to reflect new balances
+  // 3. Send success SMS to both numbers stating new balance.
   res.send('success')
 }
