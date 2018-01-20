e07f94e2a42c1f2b17f81b2535bbc107ba4edad9
rinoma <ganzo.s@libero.it>
2018-01-20 16:04:33 +0000

    created the ussue coins panel

--- src/components/IssueCoins.js
+++ src/components/IssueCoins.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, InputGroup, FormControl, DropdownButton, MenuItem, Button, Col } from 'react-bootstrap';
 import AccountPanel from './AccountPanel';
 
 class IssueCoins extends Component {
@@ -8,11 +9,26 @@ class IssueCoins extends Component {
         title='Issue Coins'
         style={this.props.containerStyle}
       >
-        <div>
-          <p>Children go here</p>
-          <p>Children go here</p>
-          <p>Children go here</p>
-          <p>Children go here</p>
+        <div style={styles.div}>
+        <Form style={styles.form}>
+          <Col xs={6} md={4}>
+        		<FormGroup controlId="enterAmount" style={styles.enterAmount}>
+        			<InputGroup.Addon>$</InputGroup.Addon>
+        			<FormControl type="text" placeholder="Enter Amount" />
+        		</FormGroup>{' '}
+          </Col>
+          <Col xs={6} md={4}>
+            <FormGroup controlId='issueType' style={styles.issueType}>
+              <FormControl componentClass="select" placeholder="Issue Type">
+                <option value="Loan" selected>Loan</option>
+                <option value="CashBuy">Cash Buy</option>
+              </FormControl>
+            </FormGroup>{' '}
+          </Col>
+          <Col xsHidden md={4}>
+      		    <Button style={styles.submitButton} type="submit">Submit</Button>
+          </Col>
+      	</Form>
         </div>
       </AccountPanel>
     );
@@ -20,7 +36,25 @@ class IssueCoins extends Component {
 }
 
 const styles = {
-
+  div: {
+    padding: '20px',
+  },
+  form: {
+    // display: 'flex',
+    // flexDirection: 'row',
+  },
+  enterAmount: {
+    display: 'flex',
+    flex: 'row',
+  },
+  issueType: {
+    // display: 'flex',
+    // flex: 'inline-block',
+  },
+  submitButton: {
+    // display: 'flex',
+    // flex: 'inline-block',
+  }
 };
 
 export default IssueCoins;
