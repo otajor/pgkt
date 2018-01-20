98d1ee94430a4aa871af388c8f1046c176a82d1c
otajor <oturnermajor@gmail.com>
2018-01-20 19:08:09 +0000

    fix fetchedUnusedAddress

--- db/fetchUnusedAddress.js
+++ db/fetchUnusedAddress.js
@@ -4,7 +4,10 @@ const fetchUnusedAddress = () =>
   knex('eth_addresses')
   .select(['address', 'private_key'])
   .whereNull('telephone')
+  .then((res) => res[0])
   .then(({ address, private_key }) => ({ address: address, privateKey: private_key }))
   .catch(console.log)
 
 module.exports = fetchUnusedAddress
+
+fetchUnusedAddress().then(console.log)
