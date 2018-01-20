31f06509502732ddca234b0dbb01675ef92ea14e
rinoma <ganzo.s@libero.it>
2018-01-20 16:31:24 +0000

    change to repay debt panel

--- src/components/RepayDebt.js
+++ src/components/RepayDebt.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, InputGroup, FormControl, Button, Col } from 'react-bootstrap';
 import AccountPanel from './AccountPanel';
 
 class RepayDebt extends Component {
@@ -8,11 +9,18 @@ class RepayDebt extends Component {
         title='Repay Debt'
         style={this.props.containerStyle}
       >
-        <div>
-          <p>Children go here</p>
-          <p>Children go here</p>
-          <p>Children go here</p>
-          <p>Children go here</p>
+        <div style={styles.div}>
+          <Form>
+            <Col md={6} md={6}>
+              <FormGroup controlId="enterAmount" style={styles.enterAmount}>
+                <InputGroup.Addon>$</InputGroup.Addon>
+              <FormControl type="number" placeholder="Enter Amount" />
+              </FormGroup>{' '}
+            </Col>
+            <Col md={6} md={6}>
+                <Button type="confirm">Confirm</Button>
+            </Col>
+          </Form>
         </div>
       </AccountPanel>
     );
@@ -20,7 +28,13 @@ class RepayDebt extends Component {
 }
 
 const styles = {
-
+  div: {
+    padding: '20px',
+  },
+  enterAmount: {
+    display: 'flex',
+    flex: 'row',
+  },
 };
 
 export default RepayDebt;
