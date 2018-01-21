f92ce2d83ac945af9cb7aff48f471945126aa13a
otajor <oturnermajor@gmail.com>
2018-01-21 11:35:22 +0000

    update balance and telephone on create account

--- createAccount.js
+++ createAccount.js
@@ -1,7 +1,9 @@
 const sendSMS = require('./sms/index')
 const fetchUnusedAddress = require('./db/fetchUnusedAddress.js')
 const updateAddressTelephone = require('./db/updateAddressTelephone.js')
+const updateAddressBalance = require('./db/updateAddressBalance.js')
 const requestAccount = require('./web3/requestAccount.js')
+const Bluebird = require('bluebird')
 
 const createAccount = ({ socket }) => ({ req, res, telephone }) => {
   const telephoneInt = telephone.replace('+', '')
@@ -9,7 +11,10 @@ const createAccount = ({ socket }) => ({ req, res, telephone }) => {
     .then(({ address, privateKey }) => {
       return requestAccount({ address, privateKey, telephone: telephoneInt })
       .then((hash) => {
-        return updateAddressTelephone({ privateKey, telephone: telephoneInt, balance: 500 })
+        return Bluebird.props({
+          telephone: updateAddressTelephone({ privateKey, telephone: telephoneInt }),
+          balance: updateAddressBalance({ privateKey, balance: 500 })
+        })
       })
       .then((res) => {
         sendSMS(telephoneInt, 'Your account has been created! Balance is 5.00KT')
